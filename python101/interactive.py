ef7955eef7ff0d689df5e2ce03a2be4572212ac3
aiya160701 <aiya160701@gmail.com>
2024-06-21 02:10:55 +0000

    First session

---
+++ python101/interactive.py
@@ -0,0 +1,3 @@
+name = input("What is your name?: ")
+name_length = len(name)
+print("hello there", name, "Your name is", name_length, "characters long")
---
+++ python101/math.py
@@ -0,0 +1,2 @@
+division=7/2
+print(division)
